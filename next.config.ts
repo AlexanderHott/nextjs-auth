78c4ed496f7fd1e46c3bc07b29a90815d8b30d4d
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-11-03 16:07:57 -0500

    fix turso issue

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import type { NextConfig } from "next";
+import "./src/env.js";
+
+const config = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+} satisfies NextConfig;
+
+export default config;
